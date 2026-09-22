44cca0b98798e5ef2237659871ff4f3af8529524
armin-akhtarzand <armin.akhtarzand@outlook.com>
2026-09-22 18:35:18 +0200

    fix: change URL name

--- src/service/authService.ts
+++ src/service/authService.ts
@@ -1,11 +1,11 @@
 import type { LoginRequest, TokenResponse } from "../types/auth";
 
-const API_BASE = import.meta.env.VITE_API_BASE_URL;
+const API_URL = import.meta.env.VITE_AUTH_API_URL;
 const TOKEN_KEY = "access_token";
 
 export async function login(credentials: LoginRequest): Promise<TokenResponse>{
 
-    const response = await fetch(`${API_BASE}/auth/login`,{
+    const response = await fetch(`${API_URL}/auth/login`,{
         method: "POST",
         headers: {
             "Content-type": "application/json"
