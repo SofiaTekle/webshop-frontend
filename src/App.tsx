8ccf374e1070cace238877dfebc7d6ab21aa6b75
anastevanovic <Ana.Stevanovic@iths.se>
2026-09-24 09:12:42 +0200

    Add welcome page after login

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import Footer from "./components/Footer";
 import { Route, Routes } from "react-router-dom";
 import ProductPage from "./pages/ProductPage";
 import LoginPage from "./pages/LoginPage";
+import WelcomePage from "./pages/WelcomePage";
 
 function App() {
   return (
@@ -10,7 +11,8 @@ function App() {
       <Header />
       <Routes>
         <Route path="/products" element={<ProductPage />} />
-        <Route path="/login" element={<LoginPage/>} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/welcome" element={<WelcomePage />} />
       </Routes>
 
       <Footer />
